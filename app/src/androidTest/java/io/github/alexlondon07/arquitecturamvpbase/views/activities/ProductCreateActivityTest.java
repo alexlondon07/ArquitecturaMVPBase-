9726e76fc7d065191eedabb328221a1df59c06cf
Alexander Londoño Espejo <alexlondon07@gmail.com>
2017-10-08 12:39:01 -0500

    Add CardView fos show Costumers List

---
+++ app/src/androidTest/java/io/github/alexlondon07/arquitecturamvpbase/views/activities/ProductCreateActivityTest.java
@@ -0,0 +1,114 @@
+package io.github.alexlondon07.arquitecturamvpbase.views.activities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.github.alexlondon07.arquitecturamvpbase.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ProductCreateActivityTest {
+
+    @Rule
+    public ActivityTestRule<ProductCreateActivity> mActivityTestRule = new ActivityTestRule<>(ProductCreateActivity.class);
+
+    @Test
+    public void productCreateActivityTest() {
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.activity_create_product_description),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        editText.check(matches(withText("")));
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.activity_create_product_quantity),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        editText2.check(matches(withText("")));
+
+        ViewInteraction editText3 = onView(
+                allOf(withId(R.id.activity_create_product_price),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        editText3.check(matches(withText("")));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.activity_create_product_button_create),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                4),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.activity_create_product_button_create),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                4),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction textInputEditText = onView(
+                withId(R.id.activity_create_product_name));
+        textInputEditText.perform(scrollTo(), click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
